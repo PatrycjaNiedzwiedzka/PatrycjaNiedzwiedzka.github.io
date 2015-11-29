62a586913e173e3a42d5a80f580836233e6163f3
PatrycjaNiedzwiedzka <p.niedzwiedzka@gmail.com>
2015-11-29 22:58:01 +0100

    Issue with sync

--- gulpfile.js
+++ gulpfile.js
@@ -13,16 +13,16 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 var browserSync = require('browser-sync').create();
 
-// Task for building blog when something changed:
-//gulp.task('build', shell.task(['bundle exec jekyll build --watch']));
-// Or if you don't use bundle:
-gulp.task('build', shell.task(['jekyll build --watch']));
+gulp.task('bower', function() {
+    gulp.src('./bower_components/**/dist/**/*.*').pipe(gulp.dest('./vendor'))
+});
 
-// Task for serving blog with Browsersync
+gulp.task('build', ['bower'], shell.task(['jekyll build --watch']));
+
 gulp.task('serve', function () {
     browserSync.init({server: {baseDir: '_site/'}});
-    // Reloads page when some of the already built files changed:
+    
     gulp.watch('_site/**/*.*').on('change', browserSync.reload);
 });
 
-gulp.task('default', ['build', 'serve']);
+gulp.task('default', ['bower', 'build', 'serve']);
