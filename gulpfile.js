ea47aaf746c8c642e3f100fa5a40198904037019
PatrycjaNiedzwiedzka <p.niedzwiedzka@gmail.com>
2015-11-29 20:41:08 +0100

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+//sudo apt-get install nodejs-legacy
+//sudo npm init
+//sudo npm install -g gulp
+//sudo npm install --save-dev gulp-shell lodash gulp browser-sync
+//And create a gulpfile.js with:
+
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+var browserSync = require('browser-sync').create();
+
+// Task for building blog when something changed:
+//gulp.task('build', shell.task(['bundle exec jekyll build --watch']));
+// Or if you don't use bundle:
+gulp.task('build', shell.task(['jekyll build --watch']));
+
+// Task for serving blog with Browsersync
+gulp.task('serve', function () {
+    browserSync.init({server: {baseDir: '_site/'}});
+    // Reloads page when some of the already built files changed:
+    gulp.watch('_site/**/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['build', 'serve']);
